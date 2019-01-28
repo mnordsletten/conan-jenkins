c328e1619a534372f326d6bde3952100186c4f6f
Martin Nordsletten <mnordsletten@gmail.com>
2019-01-28 13:49:46 +0100

    multiple jobs

--- functions.groovy
+++ functions.groovy
@@ -1,13 +1,33 @@
 #!/usr/bin/env groovy
 
-def funcA() {
-   //sh "..."
-   echo "funcA"
+def build(versions, architectures, build_types) {
+   def versions = "${params.Versions}".replaceAll("\\s", "").split(',')
+   def architectures = "${params.Architectures}".replaceAll("\\s", "").split(',')
+   def build_types = "${params.Build_types}".replaceAll("\\s", "").split(',')
+
+   def builds = [:]
+
+   for (ver in versions) {
+     for (arch in architectures) {
+       for (build in build_types) {
+         String buildName = "${build}-${arch}-${ver}"
+
+         builds[buildName] = {
+           node('conan_pipe_worker') {
+             stage(buildName) {
+               echo buildName
+             }
+           }
+         }
+       }
+     }
+   }
+   parallel builds
+ }
 }
 
-def funcB(input) {
-   //sh "..."
-   echo input
+def upload(target) {
+   echo "Uploading to: ${target}"
 }
 
 return this;
